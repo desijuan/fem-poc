bafcaf165c22c604072e2d66bf821069e2d27e30
Juan Desimoni <juan.desimoni@codex.com.br>
2025-05-03 22:43:47 -0300

    Small refactor
    Introduce Gpa interface which uses c_allocator for ReleaseSmall and the
    debug allocator for the other cases.

--- src/main.zig
+++ src/main.zig
@@ -4,11 +4,31 @@ const WoodPole = @import("WoodPole.zig");
 const Matrix = @import("Matrix.zig");
 const Mesh = @import("mesh/Mesh.zig");
 
-pub fn main() !void {
-    var gpa_instance = std.heap.DebugAllocator(.{ .safety = true }){};
-    defer _ = gpa_instance.deinit();
+const Gpa = switch (@import("builtin").mode) {
+    .ReleaseSmall => struct {
+        pub inline fn allocator() std.mem.Allocator {
+            return std.heap.c_allocator;
+        }
+    },
 
-    const gpa = gpa_instance.allocator();
+    else => struct {
+        var da_inst = std.heap.DebugAllocator(.{ .safety = true }){};
+
+        pub fn allocator() std.mem.Allocator {
+            return da_inst.allocator();
+        }
+
+        pub fn deinit() std.heap.Check {
+            return da_inst.deinit();
+        }
+    },
+};
+
+pub fn main() !void {
+    defer if (comptime @hasDecl(Gpa, "deinit"))
+        std.debug.print("da_deinit: {}\n", .{Gpa.deinit()});
+
+    const gpa = Gpa.allocator();
 
     const pole = WoodPole{
         .height = 1.036e1,
@@ -25,17 +45,17 @@ pub fn main() !void {
     const mesh: Mesh = try pole.buildMesh(gpa);
     defer mesh.deinit(gpa);
 
-    //
-    // wip ...
-    //
-    const m_K, const v_f, const v_v = blk: {
-        const n_nodes: u32 = @intCast(mesh.nodes.len * 6);
+    std.debug.print("{}", .{mesh.mat_props[0]});
+    std.debug.print("{}", .{mesh.beams[0]});
 
-        const K = try Matrix.init(gpa, n_nodes, n_nodes);
+    const m_K, const v_f, const v_v = blk: {
+        const n_eqs: u32 = @intCast(mesh.nodes.len * 6);
+
+        const K = try Matrix.init(gpa, n_eqs, n_eqs);
         errdefer K.deinit(gpa);
-        const v = try Matrix.init(gpa, n_nodes, 1);
+        const v = try Matrix.init(gpa, n_eqs, 1);
         errdefer v.deinit(gpa);
-        const f = try Matrix.init(gpa, n_nodes, 1);
+        const f = try Matrix.init(gpa, n_eqs, 1);
         errdefer f.deinit(gpa);
 
         break :blk [3]Matrix{ K, v, f };
@@ -43,12 +63,12 @@ pub fn main() !void {
     defer for ([3]Matrix{ m_K, v_f, v_v }) |m| m.deinit(gpa);
 
     const m_ek, const v_ef = blk: {
-        const n_eq: u32 = 12;
+        const n_dofs: u32 = Mesh.DOFS;
 
-        const ek = try Matrix.init(gpa, n_eq, n_eq);
+        const ek = try Matrix.init(gpa, n_dofs, n_dofs);
         errdefer ek.deinit(gpa);
 
-        const ef = try Matrix.init(gpa, n_eq, 1);
+        const ef = try Matrix.init(gpa, n_dofs, 1);
         errdefer ef.deinit(gpa);
 
         break :blk [2]Matrix{ ek, ef };
@@ -67,9 +87,17 @@ pub fn main() !void {
 
             const beam_idx: u32 = @intCast(idx);
 
+            std.debug.print("beam_idx: {}\n", .{beam_idx});
+
             try mesh.calcLocalKforBeam(beam_idx, m_ek, v_ef);
 
             std.debug.print("m_ek:\n{}", .{m_ek});
+
+            //
+            // TODO: Accumular (m_ek, v_ef) en (m_K, v_f).
+            //
+            // TODO: Revisar lo que sigue.
+            //
 
             mesh.getEquationIndicesForBeam(beam_idx, &eq_idxs);
 
@@ -85,7 +113,7 @@ pub fn main() !void {
         }
     }
 
-    std.debug.print("m_K:\n{}", .{m_K});
+    // std.debug.print("m_K:\n{}", .{m_K});
 }
 
 comptime {
